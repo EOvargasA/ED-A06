5f55f9baf773ad90ad914a905f10fb72dd0fcea1
Osiris <Osiris@Osi.lan>
2017-09-07 22:12:30 -0500

    algo

---
+++ src/nodos/Nodo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nodos;
+
+/**
+ *
+ * @author Osiris
+ */
+public class Nodo {
+       int dato;
+       
+       public Nodo siguienteNodo;
+}
